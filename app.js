82cd4fbd27ce3887a14cff655eaeb2643a1009dd
UgurKumcu <ugur.kumcu@boun.edu.tr>
2022-10-13 00:35:16 +0300

    Player choice and Invalid selection handling are done.

---
+++ app.js
@@ -0,0 +1,31 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = () => {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  // Invalid selection case
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    const message = `Invalid choice! We choose ${DEFAULT_CHOICE} for you!`;
+    alert(message);
+    return DEFAULT_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game is starting...");
+  const playerSelection = getPlayerChoice();
+});
